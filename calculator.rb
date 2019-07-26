1080af6589aa523aa92aa6f54c86fb49ff25f99b
benevolent-demo-1612 <tevinsherman@yahoo.com>
2019-07-26 22:29:33 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,4 +4,4 @@ puts second_number = 2
 puts sum = first_number + second_number
 puts difference = first_number - second_number
 puts product = first_number * second_number
-puts quotient = first_number 
+puts quotient = first_number / second_number
